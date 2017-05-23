6447b71fe7923d78a61145802d28b68582a2eb8b
unknown <brucoletinha@gmail.com>
2017-05-23 01:40:34 -0300

    codigos prontos

--- apis/battery-status/js/index.js
+++ apis/battery-status/js/index.js
@@ -3,6 +3,8 @@
 let bateria = document.getElementById('bateria');
 let nivel = document.getElementById('nivel');
 let status = document.getElementById('status');
+let tempo = document.getElementById('tempo');
+let imagem = document.getElementById('imagem');
 
 let battery = navigator.getBattery();
 
@@ -11,20 +13,40 @@ updateBatteryStatus(battery);
 function updateBatteryStatus(battery) {
 
 	let nivelBateria = battery.level * 100;
-
-	let textoBateria = nivelBateria + "%";
+    //let tempoBateriaCarregar = battery.chargingTime;
+    //let tempoBateriaDescarregar = battery.dischargingTime;
+	let textoBateria = "Nível de bateria do guerreiro:" + nivelBateria + "%";
+    //let textoTempoCarregar = tempoBateriaCarregar + "segundos";
+    //let textoTempoDescarregar = tempoBateriaDescarregar + "segundos";
+    let textoStatusCarregando = "Carregando forças...";
+    let textoStatusDescarregando = "Descarregando golpes...";
 
 	nivel.textContent = textoBateria;
+    //tempo.textContent = textoTempoCarregar;
+    //tempo.textContent = textoTempoDescarregar;
 
     if (battery.charging){
         nivel.classList.remove('bateriaBaixa');
         nivel.classList.remove('descarregando');
-    } else {
-        if (battery.level < 0.50){
+        imagem.src = "imagens/bateriacarregada.png";
+        status.textContent = textoStatusCarregando;
+    } 
+    else {
+        status.textContent = textoStatusDescarregando;
+        if (battery.level < 0.25){
             nivel.classList.remove('descarregando');
             nivel.classList.add('bateriaBaixa');
-        } else {
+            imagem.src = "imagens/bateriaruim.png";
+        } 
+        if (battery.level > 0.25 && battery.level < 0.71) {
+            nivel.classList.add('bateriaMedia');
             nivel.classList.add('descarregando');
+            imagem.src = "imagens/bateriamedia.png";
+        }  
+        else {
+            nivel.classList.add('descarregando');
+            nivel.classList.add('bateriaAlta');
+            imagem.src = "imagens/bateriaboa.png";
         }
     }  
 }
@@ -42,6 +64,14 @@ battery.then(function(battery) {
     //evento disparado quando o nível de bateria é alterado
     battery.onlevelchange = function () {
     	updateBatteryStatus(battery);
+    };
+
+    battery.onchargingTime = function () {
+        updateBatteryStatus(battery);
+    };
+
+    battery.ondischargingTime = function () {
+        updateBatteryStatus(battery);
     };
 
 });
--- apis/navigation-timing/js/index.js
+++ apis/navigation-timing/js/index.js
@@ -1,9 +1,18 @@
 //NAVIGATION TIMING API
+
+let tempoconectar = document.getElementById('tempoconectar');
+let tempocarregar = document.getElementById('tempocarregar');
 //tempo de carregamento da página
 let horaAtual = new Date().getTime();
 let tempoDeCarregamento = horaAtual - performance.timing.navigationStart;
 
 let tempoDeConexao = performance.timing.responseEnd - performance.timing.requestStart;
 
-console.log("Tempo de carregamento da página: " + tempoDeCarregamento + "ms");
-console.log("Tempo de conexão: " + tempoDeConexao + "ms");
+//console.log("Tempo de carregamento da página: " + tempoDeCarregamento + "ms");
+//console.log("Tempo de conexão: " + tempoDeConexao + "ms");
+
+let textoTempoConexao = "Tempo de Conexão com a Terra: " + tempoDeConexao + " ms";
+let textoTempoCarregamento = "Tempo de Carregamento da conexão: " + tempoDeCarregamento + " ms";
+
+tempoconectar.textContent = textoTempoConexao;
+tempocarregar.textContent = textoTempoCarregamento;
