3100c23dd96fa649ebd49f52725ae240d72b4827
Flavio Coutinho <fegemo@gmail.com>
2021-07-02 16:48:57 -0300

    Restructures apis.json and uses content hydration

--- assets/galeria.js
+++ assets/galeria.js
@@ -1,37 +1,11 @@
-export default function inicializaGaleria() {
-  let $sm = 480;
-  let $md = 768;
-
-  function resizeThis() {
-     const $imgH = $('.middle img').width();
-     if ($(window).width() >= $sm) {
-        $('.left,.right,.section').css('height', $imgH);
-     } else {
-        $('.left,.right,.section').css('height', 'auto');
-     }
-  }
-
-  resizeThis();
-
-  $(window).resize(function(){
-     resizeThis();
-  });
-
-  $(window).scroll(function() {
-     $('.section').each(function(){
-        var $elementPos = $(this).offset().top;
-        var $scrollPos = $(window).scrollTop();
-
-        var $sectionH = $(this).height();
-        var $h = $(window).height();
-        var $sectionVert = (($h/2)-($sectionH/4));
-
-
-        if (($elementPos - $sectionVert) <= $scrollPos && ($elementPos - $sectionVert) + $sectionH > $scrollPos) {
-           $(this).addClass('animate');
-        } else {
-           $(this).removeClass('animate');
-        }
-     });
-  });
+const options = {
+   threshold: 1
 }
+
+const callback = entries => {
+   entries.forEach(entry => {
+      entry.target.classList.toggle('animate', entry.isIntersecting)
+   })
+}
+
+export const observer = new IntersectionObserver(callback, options)
