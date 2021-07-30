fbf597d5532fb48e62da6e3759aa2aebfcc766f3
eusougz <guilhermegms209@gmail.com>
2021-07-29 22:23:46 -0300

    Seleção de música + mensagem "Press to start"

--- apis/gamepad-vibration/scripts/engine.js
+++ apis/gamepad-vibration/scripts/engine.js
@@ -1,100 +1,114 @@
-import Music from './music.js'
+import Music from './music.js';
 
 export class GameEngine {
-    /**
-     * @param {List[]} assetList - ['name', 'path']
-     * @param {Function} init - () => {}
-     * @param {Function} updateLoop - (dt) => {}
-     * @param {Function} drawLoop - (dt) => {}
-     */
-    constructor(init = null, updateLoop = null, drawLoop = null) {
-        this.updateLoop = updateLoop
-        this.drawLoop = drawLoop
-        this.init = init
-        this.lastupd = -1
-        this.musics = {}
-        this.assets = {}
+  /**
+   * @param {List[]} assetList - ['name', 'path']
+   * @param {Function} init - () => {}
+   * @param {Function} updateLoop - (dt) => {}
+   * @param {Function} drawLoop - (dt) => {}
+   */
+  constructor(init = null, updateLoop = null, drawLoop = null) {
+    this.updateLoop = updateLoop;
+    this.drawLoop = drawLoop;
+    this.init = init;
+    this.lastupd = -1;
+    this.musics = {};
+    this.assets = {};
 
-        this.canvas = document.querySelector('canvas')
-        this.ctx = this.canvas.getContext('2d')
+    this.canvas = document.querySelector('canvas');
+    this.ctx = this.canvas.getContext('2d');
+  }
+
+  loadAssets(assetList = []) {
+    this.assets = {};
+
+    const load = (src) => {
+      const img = new Image();
+      img.src = src;
+      return img;
+    };
+
+    for (const [assetName, assetPath] of assetList) {
+      this.assets[assetName] = load(assetPath);
     }
+  }
 
-    loadAssets(assetList = []) {
-        this.assets = {}
+  addAssets(assetList = []) {
+    const load = (src) => {
+      const img = new Image();
+      img.src = src;
+      return img;
+    };
 
-        const load = (src) => {
-            const img = new Image()
-            img.src = src
-            return img
-        }
-
-        for (const [assetName, assetPath] of assetList) {
-            this.assets[assetName] = load(assetPath)
-        }
+    for (const [assetName, assetPath] of assetList) {
+      this.assets[assetName] = load(assetPath);
     }
+  }
 
-    loadSounds(musicList = []) {
-        this.musics = {}
+  loadSounds(musicList = []) {
+    this.musics = {};
 
-        const load = (musicName) => {
-            const audio = new Music(musicName)
-            return audio
-        }
+    const load = (musicName) => {
+      const audio = new Music(musicName);
+      return audio;
+    };
 
-        for (const musicName of musicList) {
-            this.musics[musicName] = load(musicName)
-        }
+    for (const musicName of musicList) {
+      this.musics[musicName] = load(musicName);
     }
+  }
 
-    allSoundsLoaded() {
-        return Object.keys(this.musics).reduce((acc, k) => acc && this.musics[k].isReady(), true)
+  allSoundsLoaded() {
+    return Object.keys(this.musics).reduce(
+      (acc, k) => acc && this.musics[k].isReady(),
+      true
+    );
+  }
+
+  run() {
+    this.setUpCanvasSize();
+    this.setUpLoops();
+  }
+
+  setUpCanvasSize() {
+    this.ctx.canvas.width = window.innerWidth;
+    this.ctx.canvas.height = window.innerHeight;
+
+    window.addEventListener('resize', (e) => {
+      this.ctx.canvas.width = e.target.innerWidth;
+      this.ctx.canvas.height = e.target.innerHeight;
+    });
+  }
+
+  setUpLoops() {
+    this.lastupd = Date.now();
+    const loop = () => {
+      const now = Date.now();
+      const dt = now - this.lastupd; // ms
+      this.lastupd = now;
+
+      this.#updateLoop(dt);
+      this.#drawLoop(dt);
+
+      window.requestAnimationFrame(loop);
+    };
+
+    if (this.init) {
+      this.init();
     }
+    window.requestAnimationFrame(loop);
+  }
 
-    run() {
-        this.setUpCanvasSize()
-        this.setUpLoops()
+  // loops
+  #updateLoop(dt) {
+    if (this.updateLoop) {
+      this.updateLoop(dt);
     }
+  }
 
-    setUpCanvasSize() {
-        this.ctx.canvas.width = window.innerWidth
-        this.ctx.canvas.height = window.innerHeight
-    
-        window.addEventListener('resize', e => {
-            this.ctx.canvas.width = e.target.innerWidth
-            this.ctx.canvas.height = e.target.innerHeight
-        })
+  #drawLoop(dt) {
+    if (this.drawLoop) {
+      this.drawLoop(dt);
     }
-
-    setUpLoops() {
-        this.lastupd = Date.now()
-        const loop = () => {
-            const now = Date.now()
-            const dt = now - this.lastupd // ms
-            this.lastupd = now
-
-            this.#updateLoop(dt)
-            this.#drawLoop(dt)
-
-            window.requestAnimationFrame(loop)
-        }
-
-        if (this.init) {
-            this.init()
-        }
-        window.requestAnimationFrame(loop)
-    }
-
-
-    // loops
-    #updateLoop(dt) {
-        if (this.updateLoop) {
-            this.updateLoop(dt)
-        }
-    }
-
-    #drawLoop(dt) {
-        if (this.drawLoop) {
-            this.drawLoop(dt)
-        }
-    }
+  }
 }
--- apis/gamepad-vibration/scripts/initial-menu.js
+++ apis/gamepad-vibration/scripts/initial-menu.js
@@ -1,75 +1,81 @@
-
-const SONGS = 2;
-let CURRENT_SONG = 1;
-const songsUlEl = document.querySelector('.popup > ul ');
+const MUSICS = 2;
+let CURRENT_MUSIC = 1;
+const musicsUlEl = document.querySelector('.popup > ul ');
+const musicsLiEls = Array.from(musicsUlEl.childNodes).filter(
+  (el) => el.tagName?.toLowerCase() == 'li'
+);
+const musics = musicsLiEls.map((musicLiEl) => musicLiEl.dataset.musicName);
+const pressToStartEl = document.querySelector('.popup .press-to-start');
 
 export class InitialMenu {
-    handleMenuGamepadEvents(){
-        return new Promise((resolve, reject) => {
-            let oldDown, oldUp, currentUp, currentDown, oldStart, currentStart;
-            [oldStart, oldUp, oldDown] = [false, false, false]
-            [currentStart,currentUp, currentDown] = [false, false, false]
-            setInterval(() => {
-                [currentStart,currentUp, currentDown] = this.getGamepadInput().filter((el,index) => index == 12 || index == 13 || index == 0)
-                if(currentUp != oldUp ){
-                    oldUp = currentUp;
-                    if(currentUp){
-                        CURRENT_SONG--
-                        if(CURRENT_SONG == 0){
-                            CURRENT_SONG = SONGS
-                        }
-                        console.log('Pressed up -> go to song' + CURRENT_SONG);
-                        this.changeSelectedSong();
-                    }
-                } else if (currentDown != oldDown ){
-                    oldDown = currentDown;
-                    if(currentDown){
-                  
-                        CURRENT_SONG++
-                        if(CURRENT_SONG == SONGS + 1){
-                            CURRENT_SONG = 1
-                        }
-                        console.log('Pressed down -> go to song' + CURRENT_SONG);
-                        this.changeSelectedSong();
-                    }
-                } else if (currentStart != oldStart ){
-                    oldStart = currentStart;
-                    if(currentStart){
-                        resolve(CURRENT_SONG);
-                    }
-                }
-                
-            }, 1000 / 10) // check 10 times per second if the button one is pressed
-        })
-    }
+  handleMenuGamepadEvents() {
+    return new Promise((resolve, reject) => {
+      let oldDown, oldUp, currentUp, currentDown, oldStart, currentStart;
+      [oldStart, oldUp, oldDown] = [false, false, false][
+        (currentStart, currentUp, currentDown)
+      ] = [false, false, false];
+      setInterval(() => {
+        [currentStart, currentUp, currentDown] = this.getGamepadInput().filter(
+          (el, index) => index == 12 || index == 13 || index == 0
+        );
+        if (currentUp != oldUp) {
+          oldUp = currentUp;
+          if (currentUp) {
+            CURRENT_MUSIC--;
+            if (CURRENT_MUSIC == 0) {
+              CURRENT_MUSIC = MUSICS;
+            }
+            console.log('Pressed up -> go to music' + CURRENT_MUSIC);
+            this.changeSelectedMusic();
+          }
+        } else if (currentDown != oldDown) {
+          oldDown = currentDown;
+          if (currentDown) {
+            CURRENT_MUSIC++;
+            if (CURRENT_MUSIC == MUSICS + 1) {
+              CURRENT_MUSIC = 1;
+            }
+            console.log('Pressed down -> go to music' + CURRENT_MUSIC);
+            this.changeSelectedMusic();
+          }
+        } else if (currentStart != oldStart) {
+          oldStart = currentStart;
+          if (currentStart) {
+            document.querySelector('.bg').style.display = 'none';
+            resolve(musics[CURRENT_MUSIC - 1]);
+          }
+        }
+      }, 1000 / 10); // check 10 times per second if the button one is pressed
+    });
+  }
 
-    handleMouseEvents(){
-        return new Promise((resolve ,reject) => {
-            Array.from(songsUlEl.childNodes).filter(el => el.tagName?.toLowerCase() == 'li').forEach(node => {
-                node.addEventListener('click',() => {
-                    CURRENT_SONG = +node.id.replace('music', '');
-                    console.log(CURRENT_SONG);
-                    resolve(CURRENT_SONG);
-                });
-            });
-        })
-    }
-
-    getGamepadInput = () => {
-        
-        let buttons = navigator.getGamepads()[0].buttons.map(b => b.pressed)
-        
-        return buttons
-    }
-
-
-    changeSelectedSong(){
-        
-        // console.log(songsUlEl.childNodes)
-        Array.from(songsUlEl.childNodes).filter(el => el.tagName?.toLowerCase() == 'li').forEach(node => {
-            // console.log(node)
-            node.id == ('music' + CURRENT_SONG) ? node.classList.add('selected') : node.classList.remove('selected');
+  handleMouseEvents() {
+    return new Promise((resolve, reject) => {
+      Array.from(musicsUlEl.childNodes)
+        .filter((el) => el.tagName?.toLowerCase() == 'li')
+        .forEach((node) => {
+          node.addEventListener('click', () => {
+            CURRENT_MUSIC = +node.id.replace('music', '');
+            document.querySelector('.bg').style.display = 'none';
+            resolve(musics[CURRENT_MUSIC - 1]);
+          });
         });
-    }
+    });
+  }
 
+  getGamepadInput = () => {
+    let buttons = navigator.getGamepads()[0].buttons.map((b) => b.pressed);
+    return buttons;
+  };
+
+  changeSelectedMusic() {
+    pressToStartEl.classList.remove('hide');
+
+    // console.log(musicsUlEl.childNodes)
+    musicsLiEls.forEach((node) => {
+      node.id == 'music' + CURRENT_MUSIC
+        ? node.classList.add('selected')
+        : node.classList.remove('selected');
+    });
+  }
 }
